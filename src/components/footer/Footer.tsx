a093b7cadf899538137ee1ce0d922b59b62152f3
doublejan <954464727@qq.com>
2020-01-30 21:09:36 +0800

    update: 完成首页头部，底部页面

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,16 @@
 // 全局footer
 import React from 'react';
+import './footer.less';
 
 function Footer() {
   return (
-    <footer className={'footer-wrap'}>Footer</footer>
+    <footer className={'footer-wrap'}>
+      <div className={'author-inform'}>
+        <span>EMAIL</span>
+        <span>GITHUB</span>
+      </div>
+      <div className={"copyright"}>Copyright © 2020 - 2020 Spirit Thought. All Rights Reserved</div>
+    </footer>
   )
 }
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,15 +6,24 @@ function Header() {
   return (
     <header className={'header-wrap'}>
       <div className={'header-title-wrap'}>
+        {/* <p className={"header-title"}>SPIRIT THOUGHT</p> */}
+        <img src="/assets/img/fire-active.svg" alt=""/>
         <p className={"header-title"}>灵思</p>
       </div>
       <div className={"header-nav-wrap"}>
-        <span className={"nav-item"}>首页</span>
-        <span className={"nav-item"}>笔记</span>
-        <span className={"nav-item"}>文章</span>
-        <span className={"nav-item"}>健康</span>
-        <span className={"nav-item"}>管理</span>
+        <div className={"nav-item"}><span className={"nav-item-span"}>HOME</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>NOTE</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>ARTICLE</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>HEALTH</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>ADMIN</span></div>
       </div>
+      {/* <div className={"header-nav-wrap"}>
+        <div className={"nav-item"}><span className={"nav-item-span"}>首页</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>笔记</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>博文</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>安全</span></div>
+        <div className={"nav-item"}><span className={"nav-item-span"}>管理</span></div>
+      </div> */}
     </header>
   )
 }
