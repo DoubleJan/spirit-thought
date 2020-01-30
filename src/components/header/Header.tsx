f27a5bb4c2310f29c06312b38c325eff8e52ad45
doublejan <954464727@qq.com>
2020-01-30 15:31:34 +0800

    fix: 修改assets目录位置，public下的文件不打包

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,20 @@
 // 头部header
 import React from 'react';
+import './header.less';
 
 function Header() {
   return (
-    <header>
-      Header
+    <header className={'header-wrap'}>
+      <div className={'header-title-wrap'}>
+        <p className={"header-title"}>灵思</p>
+      </div>
+      <div className={"header-nav-wrap"}>
+        <span className={"nav-item"}>首页</span>
+        <span className={"nav-item"}>笔记</span>
+        <span className={"nav-item"}>文章</span>
+        <span className={"nav-item"}>健康</span>
+        <span className={"nav-item"}>管理</span>
+      </div>
     </header>
   )
 }
