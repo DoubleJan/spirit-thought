84b3a1698950be4a6346dca674f8a9d85c9db6f5
doublejan <954464727@qq.com>
2020-02-05 22:32:43 +0800

    update: 优化pc端横屏health页面header显示样式

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,8 @@ function App() {
   return (
     <HashRouter>
       <div className="App">
-        {!href.includes('health') && <Header />}
+        {/* {!href.includes('health') && <Header />} */}
+        <Header />
         <div className={`${href.includes('/health') ? '' : 'page-body'}`}>
           <Switch>
             <Route exact path="/" component={Home} />
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,54 +7,62 @@ import './header.less';
 function Header() {
   const history = useHistory();
   const location = useLocation();
+  console.log('location: ', location);
+  // location.pathname.includes('health') ? null :
+  return location.pathname.includes('health') ? (
+    <div className={'health-header-title-wrap'} onClick={() => history.push('/')}>
+      <img src="/assets/img/fire-active.svg" alt="" />
+      <p className={"health-header-title"}>灵思</p>
+    </div>
+  ) : (
+      (
+        <header className={'header-wrap'}>
+          <div className={'header-title-wrap'} onClick={() => history.push('/')}>
+            <img src="/assets/img/fire-active.svg" alt="" />
+            <p className={"header-title"}>灵思</p>
+          </div>
 
-  return (
-    <header className={'header-wrap'}>
-      <div className={'header-title-wrap'} onClick={() => history.push('/')}>
-        <img src="/assets/img/fire-active.svg" alt="" />
-        <p className={"header-title"}>灵思</p>
-      </div>
-
-      <div className={"header-nav-wrap"}>
-        <div className={"nav-item"}>
-          <span
-            className={`nav-item-span ${location.pathname.includes('/note') ? 'actived-nav-span' : ''}`}
-            onClick={() => history.push('/note')}
-          >NOTE</span>
-        </div>
-        <div className={"nav-item"}>
-          <span
-            className={`nav-item-span ${location.pathname.includes('/article') ? 'actived-nav-span' : ''}`}
-            onClick={() => history.push('/article')}
-          >ARTICLE</span>
-        </div>
-        <div className={"nav-item"}>
-          <span
-            className={`nav-item-span ${location.pathname.includes('/code') ? 'actived-nav-span' : ''}`}
-            onClick={() => history.push('/code')}
-          >CODE</span>
-        </div>
-        <div className={"nav-item"}>
-          <span
-            className={`nav-item-span ${location.pathname.includes('/books') ? 'actived-nav-span' : ''}`}
-            onClick={() => history.push('/books')}
-          >BOOKS</span>
-        </div>
-        <div className={"nav-item"}>
-          <span
-            className={`nav-item-span ${location.pathname.includes('/health') ? 'actived-nav-span' : ''}`}
-            onClick={() => history.push('/health')}
-          >HEALTH</span>
-        </div>
-        <div className={"nav-item none-admin"}>
-          <span
-            className={`nav-item-span ${location.pathname.includes('/admin') ? 'actived-nav-span' : ''}`}
-            onClick={() => history.push('/admin')}
-          >ADMIN</span>
-        </div>
-      </div>
-    </header>
-  )
+          <div className={"header-nav-wrap"}>
+            <div className={"nav-item"}>
+              <span
+                className={`nav-item-span ${location.pathname.includes('/note') ? 'actived-nav-span' : ''}`}
+                onClick={() => history.push('/note')}
+              >NOTE</span>
+            </div>
+            <div className={"nav-item"}>
+              <span
+                className={`nav-item-span ${location.pathname.includes('/article') ? 'actived-nav-span' : ''}`}
+                onClick={() => history.push('/article')}
+              >ARTICLE</span>
+            </div>
+            <div className={"nav-item"}>
+              <span
+                className={`nav-item-span ${location.pathname.includes('/code') ? 'actived-nav-span' : ''}`}
+                onClick={() => history.push('/code')}
+              >CODE</span>
+            </div>
+            <div className={"nav-item"}>
+              <span
+                className={`nav-item-span ${location.pathname.includes('/books') ? 'actived-nav-span' : ''}`}
+                onClick={() => history.push('/books')}
+              >BOOKS</span>
+            </div>
+            <div className={"nav-item"}>
+              <span
+                className={`nav-item-span ${location.pathname.includes('/health') ? 'actived-nav-span' : ''}`}
+                onClick={() => history.push('/health')}
+              >HEALTH</span>
+            </div>
+            <div className={"nav-item none-admin"}>
+              <span
+                className={`nav-item-span ${location.pathname.includes('/admin') ? 'actived-nav-span' : ''}`}
+                onClick={() => history.push('/admin')}
+              >ADMIN</span>
+            </div>
+          </div>
+        </header>
+      )
+    )
 }
 
 export default Header;
